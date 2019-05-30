64b312ae481222c3da02de6a83cefa3854a8686b
Adrian Duong <adrian.duong@gmail.com>
2019-05-30 12:16:33 -0700

    xmonad: add simple XFCE config

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,21 @@
+import XMonad
+import XMonad.Config.Xfce
+import XMonad.Hooks.SetWMName
+
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+
+import XMonad.Layout.Column
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.NoBorders
+
+main = xmonad xfceConfig
+  { modMask = mod4Mask
+  , manageHook = (className =? "Xfce4-notifyd" --> doIgnore) <+> fullscreenManageHook <+> manageDocks <+> manageHook xfceConfig
+  , layoutHook = smartBorders $ fullscreenFull $ avoidStruts $ (layoutHook xfceConfig ||| Column 1 ||| Mirror (Column 1))
+  , handleEventHook = fullscreenEventHook
+  , focusFollowsMouse = False
+  , clickJustFocuses = False
+  , terminal = "xfce4-terminal"
+  }
+
