4066f165442f6e697b920db153b261b51aac2f93
Adrian Duong <adrian.duong@gmail.com>
2022-04-20 20:26:02 -0700

    Specify @latest for Go packages

--- render/install_go_binaries.sh
+++ render/install_go_binaries.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 packages=(
-  github.com/GoogleCloudPlatform/cloudsql-proxy/cmd/cloud_sql_proxy
-  github.com/aduong/gopkggraph
-  github.com/cespare/reflex
+  github.com/GoogleCloudPlatform/cloudsql-proxy/cmd/cloud_sql_proxy@latest
+  github.com/aduong/gopkggraph@latest
+  github.com/cespare/reflex@latest
   github.com/golangci/golangci-lint/cmd/golangci-lint@v1.44.2
   golang.org/x/tools/gopls@latest
   # github.com/kubernetes-sigs/kustomize # seems to be broken as of 2021-04-20
