3d9202564dfef3c3f78165b194f4c9e2f08485cd
Adrian Duong <adrian.duong@gmail.com>
2023-03-26 12:28:56 -0700

    Deal with docks in xmonad

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -9,9 +9,9 @@ import XMonad.Layout.Column
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
 
-main = xmonad xfceConfig
+main = xmonad $ docks xfceConfig
   { modMask = mod4Mask
-  , manageHook = (className =? "Xfce4-notifyd" --> doIgnore) <+> fullscreenManageHook <+> manageDocks <+> manageHook xfceConfig
+  , manageHook = (className =? "Xfce4-notifyd" --> doIgnore) <+> fullscreenManageHook <+> manageHook xfceConfig
   , layoutHook = smartBorders $ fullscreenFull $ avoidStruts $ (layoutHook xfceConfig ||| Column 1 ||| Mirror (Column 1))
   , handleEventHook = fullscreenEventHook
   , focusFollowsMouse = False
