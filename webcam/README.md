9af1a720d739404703d9830e36ec78832057c406
Adrian Duong <adrian.duong@gmail.com>
2025-11-24 12:35:16 -0800

    add webcam zoom rules

---
+++ webcam/README.md
@@ -0,0 +1,4 @@
+# Instructions
+
+1. Add /etc/udev/rules.d/90-c920-webcam.rules
+2. Add & set executable /usr/local/bin/configure-logitech-c920-webcam.sh
---
+++ webcam/configure-logitech-c920-webcam.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+exec &>> /var/log/configure-logitech-c920-webcam.log
+
+date
+echo "Setting ${DEVNAME} focus and zoom"
+env | grep -e '^DEV' -e '^ID'
+
+if ! v4l2-ctl -d "${DEVNAME}" --list-ctrls | grep -q focus; then
+  echo 'No focus, exitting early'
+  exit
+fi
+
+v4l2-ctl -d "${DEVNAME}" --set-ctrl focus_automatic_continuous=0 
+v4l2-ctl -d "${DEVNAME}" --set-ctrl focus_absolute=0,zoom_absolute=150
