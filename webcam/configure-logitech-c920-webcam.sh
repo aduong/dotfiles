a8548199bebb741f591cf2a98eede6013ee93949
Adrian Duong <adrian.duong@gmail.com>
2026-07-20 12:14:19 -0700

    add logging to c920 webcam config

--- webcam/configure-logitech-c920-webcam.sh
+++ webcam/configure-logitech-c920-webcam.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-exec &>> /var/log/configure-logitech-c920-webcam.log
+exec &>> ${LOGFILE:-/var/log/configure-logitech-c920-webcam.log}
 
 date
 echo "Setting ${DEVNAME} focus and zoom"
@@ -13,3 +13,5 @@ fi
 
 v4l2-ctl -d "${DEVNAME}" --set-ctrl focus_automatic_continuous=0 
 v4l2-ctl -d "${DEVNAME}" --set-ctrl focus_absolute=0,zoom_absolute=150
+
+echo "Set ${DEVNAME} focus=0 and zoom=150"
